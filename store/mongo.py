06e93c87c2d07dff0873f7207b83c7c4bfdf17d5
aliywedad <aliysidahmedwedad@gmail.com>
2026-01-23 22:00:31 +0000

    mongo

--- store/mongo.py
+++ store/mongo.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 client = MongoClient(
-    # "mongodb+srv://22086_db_user:niVv502YSdOIkVdk@cluster0.egwmoiy.mongodb.net/?appName=Cluster0"
     "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.10"
 )
 
